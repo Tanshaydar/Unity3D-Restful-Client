e704ebba79bb8f6d626da302e7a593ec296b8431
Tansel Altınel <tansel.altinel@esensi.com.tr>
2015-01-02 11:04:55 +0200

    Initial commit, includes nGUI import and UnityHTTP library with a test "GET" call from a public REST api. It works.

---
+++ Assets/Scripts/RestTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestTest : MonoBehaviour {
+	/*
+	 * https://github.com/andyburke/UnityHTTP
+	 */
+
+	private static string URL = "http://api.openweathermap.org/data/2.5/weather?q=London,uk";
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SomeRoutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerator SomeRoutine() {
+		Debug.Log ("Test");
+		HTTP.Request someRequest = new HTTP.Request( "get", URL);
+		someRequest.Send();
+		
+		while( !someRequest.isDone )
+		{
+			yield return null;
+		}
+		Hashtable table = someRequest.response.Object;
+		// parse some JSON, for example:
+		Debug.Log (table.ContainsKey("base"));
+		Debug.Log ((string)table["base"]);
+	}
+}
