9e78703e1d1957853f892314f1181f26009bd77d
Tansel Altınel <alirsin@gmail.com>
2015-01-06 01:04:17 +0200

    First call to localhost server. Needs a JSON parser.

---
+++ Assets/Scripts/Device.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Device {
+	private string uniqueId;
+	private string deviceName;
+	private bool isOverriden;
+	private string timeStart;
+	private string timeEnd;
+	private string notes;
+
+	public Device(){
+		uniqueId = SystemInfo.deviceUniqueIdentifier;
+		deviceName = SystemInfo.deviceName;
+	}
+	public Device (string uniqueId, string deviceName, bool isOverriden, string timeStart, string timeEnd, string notes) {
+		this.uniqueId = uniqueId;
+		this.deviceName = deviceName;
+		this.isOverriden = isOverriden;
+		this.timeStart = timeStart;
+		this.timeEnd = timeEnd;
+		this.notes = notes;
+	}
+}
---
+++ Assets/Scripts/RESTful.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RESTful : MonoBehaviour {
+
+	private string URL;
+	private string API;
+	private Device device;
+
+	void Awake() {
+		URL = "http://localhost:80/zaman";
+		API = URL + "/api/";
+		device = new Device ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		checkDevice ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool checkDevice(){
+		HTTP.Request checkDevice = new HTTP.Request("get", API + "device/123sdgas64");
+		checkDevice.Send( (request) => {
+			Debug.Log(request.response.Text);
+		});
+		return false;
+	}
+}
